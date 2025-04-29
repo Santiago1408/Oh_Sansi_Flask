42b55d4de490145fe19c1270bc93ae87b5bb6a1a
Josue <josueg4rcia@gmail.com>
2025-04-28 21:41:38 -0400

    Comienzo del proyecto

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
-
-app = Blueprint('main', __name__)
+from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'tunari_tech_srl')
