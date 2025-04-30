4fd26f781505686895e0d78119f2ff137202cc01
Josue <josueg4rcia@gmail.com>
2025-04-29 21:23:10 -0400

    Conexion a base de datos

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
+# Configuración de MySQL
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'olimpiadas'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
 from app import routes
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from app import app
-from flask import render_template
+from app import app, mysql
+from flask import render_template, jsonify
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
