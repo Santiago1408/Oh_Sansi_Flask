34517ccf2694802cd080581ce85b5d6d432ab979
floramos06 <202200303@est.umss.edu>
2025-06-02 19:37:24 -0400

    Admin reportes y visualizacion

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ app.config.from_object('config.Config')
 
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = '10734578'
 app.config['MYSQL_DB'] = 'olimpiada'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
